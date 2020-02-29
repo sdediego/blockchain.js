d453ab769071d10fb32794fedaa007e0f2873d72
Sergio de Diego <sergio.dediego@outlook.com>
2020-02-29 19:35:05 +0100

    Create BaseError and LoggerError classes

---
+++ backend/src/utils/errors.js
@@ -0,0 +1,43 @@
+/**
+ * Class-based errors for blockchain components.
+ *
+ * @file Defines custom errors for the application.
+ */
+
+/**
+ * Constructs an instance of BaseError class.
+ *
+ * Provides the ability to create a new BaseError instance
+ * with custom message and code.
+ *
+ * @access public
+ * @class
+ */
+class BaseError extends Error {
+
+  /**
+   * Constructs an instance of BaseError class.
+   *
+   * @access     public
+   * @constructs BaseError
+   *
+   * @constructor
+   * @param  {Number}    message Custom error message to provide information.
+   * @param  {String}    code    Custom error number code.
+   * @return {BaseError} Class instance.
+   */
+  constructor(message, code) {
+    message = code !== null ? `Error ${ code } ${ message }` : message;
+    super(message);
+    this.code = code;
+    this.message = message;
+  }
+}
+
+class LoggerError extends BaseError {
+  constructor(message, code = null) {
+    super(message, code);
+  }
+}
+
+export { LoggerError };

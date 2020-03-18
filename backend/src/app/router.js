c20d10e3518233c3e25d0e671a508b9eac01d6b0
Sergio de Diego <sergio.dediego@outlook.com>
2020-03-18 21:23:03 +0100

    Add get home API route

--- backend/src/app/router.js
+++ backend/src/app/router.js
@@ -3,3 +3,24 @@
  *
  * @file Defines blockchain application backend API router.
  */
+
+import express from 'express';
+import getLogger from '../utils/logger';
+
+const logger = getLogger(__filename);
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  logger.info('[API] GET home.');
+
+  return res.status(200).json({
+    status: {
+      code: res.statusCode,
+      msg: 'OK'
+    },
+    data: 'Welcome to Blockchain'
+  });
+});
+
+export default router;

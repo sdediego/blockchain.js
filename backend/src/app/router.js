cde7d9a3a27504c88eac05ca80abc9bc0f837f09
Sergio de Diego <sergio.dediego@outlook.com>
2020-03-18 21:24:53 +0100

    Add get blockchain API route

--- backend/src/app/router.js
+++ backend/src/app/router.js
@@ -5,11 +5,13 @@
  */
 
 import express from 'express';
+import Blockchain from '../blockchain/models/blockchain.model';
 import getLogger from '../utils/logger';
 
 const logger = getLogger(__filename);
 
 const router = express.Router();
+const blockchain = new Blockchain();
 
 router.get('/', (req, res, next) => {
   logger.info('[API] GET home.');
@@ -20,6 +22,18 @@ router.get('/', (req, res, next) => {
       msg: 'OK'
     },
     data: 'Welcome to Blockchain'
+  });
+});
+
+router.get('/blockchain', (req, res, next) => {
+  logger.info('[API] GET blockchain.');
+
+  return res.status(200).json({
+    status: {
+      code: res.statusCode,
+      msg: 'OK'
+    },
+    data: blockchain
   });
 });
 
